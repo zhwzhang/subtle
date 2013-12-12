e207ac02160ad2249e7465117e810bbb1153850f
Goran Petrovic <stgpetrovic@gmail.com>
2013-12-12 16:38:56 +0100

    Adding example.

---
+++ src/example.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "src/rpc_impl.h"
+#include "src/subtle.h"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "Usage: ./subtle path/to/video.mvk" << std::endl;
+    exit(1);
+  }
+
+  libsubtle::XmlRpcImpl client;
+  libsubtle::Subtle s(&client);
+  s.DownloadSubtitles("eng", argv[1]);
+}
